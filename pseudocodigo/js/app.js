96add8390b49fcd9cc7e52de7f7b918d00127ffc
luisNavasArg <lpipnavas@gmail.com>
2024-02-29 17:54:59 -0300

    variables, tipos, arrays

---
+++ pseudocodigo/js/app.js
@@ -0,0 +1,73 @@
+/*alert("Hola estamos en el frontend");
+alert(5+17);
+console.log(14-25);
+console.log(23>50);
+let nombre = "Luis";
+console.log(nombre);
+nombre=53;
+console.log(nombre);*/
+
+//Varibles
+//declaración palabra reservada let nombre-variable signo de asignación valor
+let nombre;
+//asignamos un valor
+nombre ="Luis";
+//numerica
+//declaración e inicilización
+let numero = 10.50;
+//booleana
+let llueve =false;
+let esta_conectada=true;
+//usamos la variable
+console.log(nombre);
+
+//
+nombre="Ángel";
+console.log(nombre);
+
+let base=10;
+let altura=5;
+let resultado=base*altura;
+console.log("El área del rectangulo es",resultado);
+
+//Tipos de variables
+//string
+let miNombre="Juan";
+//númerico
+let num=100000;
+let iva;
+
+resultado=num*21/100;
+let resultado2= num*0.21;
+
+console.log("El iva de "+num+ " es: " +resultado);
+console.log(typeof resultado);
+console.log(typeof nombre);
+console.log(typeof operacion);
+let op;
+op=true;
+let alumno={};
+let alumnos=[];
+console.log(typeof alumno);
+console.log(typeof alumnos);
+
+let aula=10;
+console.log(aula);
+aula=23;
+console.log(aula);
+
+const PI=3.14;
+let r=15;
+resultado =r*r*PI;
+console.log("el área del circulo es: "+resultado); 
+
+
+
+
+
+
+
+
+
+
+
